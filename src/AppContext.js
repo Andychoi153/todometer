f889f9958d4bcc6a6b3ab9dfd05279ecb1fdf9ac
hanmillee <94mil@naver.com>
2020-10-01 17:09:20 +0900

    t

--- src/AppContext.js
+++ src/AppContext.js
@@ -5,22 +5,21 @@ import { remote } from "electron";
 
 export const AppContext = createContext();
 
-
 export function getTimepercentage(nd) {
   let percentage = 1;
-  let left = 0
+  let left = 0;
 
   switch (remote.getGlobal("notificationSettings").reminderNotification) {
     case "hour":
-      percentage = 1 - ( nd.getMinutes() / 60 )
+      percentage = 1 - nd.getMinutes() / 60;
       break;
     case "halfhour":
-      left = nd.getMinutes() % 30
-      percentage = 1 - (left / 30)
+      left = nd.getMinutes() % 30;
+      percentage = 1 - left / 30;
       break;
     case "quarterhour":
-      left = nd.getMinutes() % 15
-      percentage = 1 - (left / 15)
+      left = nd.getMinutes() % 15;
+      percentage = 1 - left / 15;
       break;
     default:
       break;
@@ -39,12 +38,12 @@ export function useAppReducer() {
 export function useItems() {
   const { items } = useAppState();
 
-  const pending = items.filter(item => item.status === "pending");
-  const paused = items.filter(item => item.status === "paused");
-  const completed = items.filter(item => item.status === "completed");
-  const routine = items.filter(item => item.status === "routine");
-  const logging = items.filter(item => item.status === "logging");
-  const timePercentage = items.filter(item => item.status === "timer");
+  const pending = items.filter((item) => item.status === "pending");
+  const paused = items.filter((item) => item.status === "paused");
+  const completed = items.filter((item) => item.status === "completed");
+  const routine = items.filter((item) => item.status === "routine");
+  const logging = items.filter((item) => item.status === "logging");
+  const timePercentage = items.filter((item) => item.status === "timer");
 
   return { pending, paused, completed, routine, logging, timePercentage };
 }
@@ -58,7 +57,7 @@ const appStateReducer = (state, action) => {
     month: format(nd, "MM"),
     monthDisplay: format(nd, "MMM"),
     year: format(nd, "y"),
-    weekday: format(nd, "EEEE")
+    weekday: format(nd, "EEEE"),
   };
 
   switch (action.type) {
@@ -68,10 +67,10 @@ const appStateReducer = (state, action) => {
       return newState;
     }
     case "UPDATE_ITEM": {
-      const newItems = state.items.map(i => {
+      const newItems = state.items.map((i) => {
         if (i.key === action.item.key) {
           return Object.assign({}, i, {
-            status: action.item.status
+            status: action.item.status,
           });
         }
         return i;
@@ -83,59 +82,43 @@ const appStateReducer = (state, action) => {
     case "DELETE_ITEM": {
       const newState = {
         ...state,
-        items: state.items.filter(item => item.key !== action.item.key)
+        items: state.items.filter((item) => item.key !== action.item.key),
       };
       saveState(newState);
       return newState;
     }
     case "RESET_ALL": {
-      let updateItems = state.items
-        .map(i => {
-          if (i.status === "completed") {
-            return Object.assign({}, i, {
-              status: "routine"
-            });
-          }
-          return i;
-        });
-
-      state.items
-      .filter(item => item.status === "completed")
-      .map(i => {
-        const newItem = {
-          text: i.text,
-          key: Date.now(),
-          status: "logging"
-        };
-        updateItems = updateItems.concat(newItem);
-      });
-      const newState = { ...state, items: updateItems, date: currentDate };
-      saveState(newState);
-      return newState;
-      }
-
-    case "TIME_CHECK": {
-      nd = new Date()
-      let newTime = getTimepercentage(nd);
-      const newItems = state.items.map(i => {
-        if (i.status === "timer") {
+      let updateItems = state.items.map((i) => {
+        if (i.status === "completed") {
           return Object.assign({}, i, {
-            timePercentage: newTime
+            status: "routine",
           });
         }
         return i;
       });
-      const newState = { ...state, items: newItems};
+
+      state.items
+        .filter((item) => item.status === "completed")
+        .map((i) => {
+          const newItem = {
+            text: i.text,
+            key: Date.now(),
+            status: "logging",
+          };
+          updateItems = updateItems.concat(newItem);
+        });
+      const newState = { ...state, items: updateItems, date: currentDate };
       saveState(newState);
-      return newState
+      return newState;
     }
-    
-    case "ADD_MEMO": {
-      const newItems = state.items.map(i => {
-        if (i.key === action.item.key) {
+
+    case "TIME_CHECK": {
+      nd = new Date();
+      let newTime = getTimepercentage(nd);
+      const newItems = state.items.map((i) => {
+        if (i.status === "timer") {
           return Object.assign({}, i, {
-            status: action.item.status,
-            memo: action.item.memo
+            timePercentage: newTime,
           });
         }
         return i;
@@ -157,7 +140,7 @@ export function AppStateProvider({ children }) {
     let timer = {
       key: Date.now(),
       status: "timer",
-      timePercentage: 1
+      timePercentage: 1,
     };
 
     initialState = {
@@ -168,8 +151,8 @@ export function AppStateProvider({ children }) {
         month: format(nd, "MM"),
         monthDisplay: format(nd, "MMM"),
         year: format(nd, "y"),
-        weekday: format(nd, "EEEE")
-      }
+        weekday: format(nd, "EEEE"),
+      },
     };
   }
 
